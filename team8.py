b6bc3cea6c6e25dcbe3403508a8764e40db37ce7
dayanaraA <dayanaraA@ccaeagles.org>
2018-07-26 08:07:01 -0400

    Added my startegy for Team 8

--- team8.py
+++ team8.py
@@ -7,28 +7,11 @@
 ####
 
 team_name = 'Dee' # Only 10 chars displayed.
-strategy_name = 'Threes Company'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'The Rochford'
+strategy_description = 'Always collude except when the other team''s betrays then I betray except 1/7 of the time I can change my mind andcollude '
+import random
     
 def move(my_history, their_history, my_score, their_score):
-    '''    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-    count = 0
-    for i in range(-3,0):
-        if their_history[i] == 'b':
-            count +=1
-    
-    if count >= 2:
-        return 'b'
-    else:
-        return 'c'
-        
-        
-        
-
-
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -36,9 +19,23 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
+    '''    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    if len(my_history)==0:
+        return 'c'
+    else:
+        if their_history[-1]=='b':
+            if random.randint(1,7)==1:
+                return 'c'
+            else:
+                return 'b'
+        else:
+            return 'c'    
+        
+    
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
